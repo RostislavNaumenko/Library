d404b022fea85167dae681ed2fe82cd5b8c3e3cc
AllaNovytska <anovytska2022@gmail.com>
2024-06-27 18:53:56 +0200

    BookService

--- src/service/BookService.java
+++ src/service/BookService.java
@@ -4,6 +4,9 @@ import model.Book;
 import repository.BookRepository;
 import util.MyList;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BookService {
     private final BookRepository bookRepository;
 
@@ -14,12 +17,26 @@ public class BookService {
 
     //Add books
 
-    public Book addBook(String title, String author){
-        //TODO(Alla) Проверить если существует эта книга в нашем списке ( с таким именем и автором)
-        Book book = bookRepository.addBook(title, author);
 
-        return book;
+
+
+        //TODO(Alla) Проверить если существует эта книга в нашем списке ( с таким именем и автором)
+    // Поиск по названию и автору:
+
+    public Book searchBooks(String title, String author) {
+        for (Book book : bookRepository.getAllBooks()) {
+            if (book.getTitle().equalsIgnoreCase(title) && book.getAuthor().equalsIgnoreCase(author)) {
+                return book;
+            }
+        }
+        return null; // Книга не найдена
     }
+
+
+
+
+
+
 
     // Get books
 
@@ -41,17 +58,44 @@ public class BookService {
 
     //Take book
 
-    public boolean takeBook (int bookId){
-        //TODO(Alla) Проверить не взята уже эта книга
-        return  bookRepository.takeBook(bookId);
+    //Поиск по ID: Если у вас есть уникальный идентификатор
+    // (например, bookID),
+    // вы можете использовать его для поиска книги:
+    public Book searchBookByID(int bookID) {
+        return bookRepository.getBookById(bookID);
     }
+
+    public boolean bookIsTaken (int bookId){
+        //TODO(Alla) Проверить не взята уже эта книга
+        Book book = bookRepository.getBookById(bookId);
+        if (book==null) {
+            return  true; // уточнить
+        }
+        return  book.isTaken();
+    }
+
+
 
     //Remove book
 
+
     public Book removeBook(int bookId){
-        //TODO (Alla) Проверить на существование такого id book
+        //TODO (Alla) Проверить на существование такого
+        // id book
+        Book book = bookRepository.getBookById(bookId);
+        //return book !=null; // нужно решить что правильно возвращать это (в этой строке !=null) или в строке ниже
         return bookRepository.removeBook(bookId);
     }
 
 
+//Использование Map: Мы также можем использовать Map для хранения книг по уникальному ключу
+    // (например, по названию и автору). Это ускорит поиск:
+
+    private Map<String, Book> bookMap = new HashMap<>();
+
+    // Убедитесь, что у вас есть методы для добавления книг (addBook()) и проверки
+    // наличия пользователя (userLogin()), как в вашем коде.
+
+
 }
+
