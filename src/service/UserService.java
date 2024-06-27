17a8e191a7796836c5c795258b2cb9b481d1ac98
AllaNovytska <anovytska2022@gmail.com>
2024-06-27 19:14:33 +0200

    Validation email, password

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -28,4 +28,32 @@ public class UserService {
         return user;
     }
 
-}
+
+        // 1.Валидация email:
+        public class EmailValidation {
+            public static boolean isValidEmailAddress(String email) {
+                boolean result = true;
+                try {
+                    Email emailAddr = new emailAddr(email);
+                    emailAddr.validate();
+                } catch (AddressException ex) {
+                    result = false;
+                }
+                return result;
+            }
+        }
+        // 2.Валидация password:
+           String password = "MySecurePassword123!";
+
+        boolean isValidPassword = password.matches("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&*!])[A-Za-z\\d@#$%^&*!]{8,}$");
+        //Это регулярное выражение проверяет, что пароль содержит
+        // как минимум одну букву, одну цифру и один специальный символ, и имеет длину не менее 8 символов
+
+
+        // 3.Валидация name:
+          String name = "John"; // Пример имени
+        boolean isValidName = name.matches("^[A-Z][a-zA-Z]*$");
+        // Это регулярное выражение проверяет, что имя начинается с большой буквы и содержит только буквы
+
+
+    }
