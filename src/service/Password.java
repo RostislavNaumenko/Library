dadbbec8cd657b7636a48460a719bd8012bf665c
AllaNovytska <anovytska2022@gmail.com>
2024-06-28 18:26:02 +0200

    alla/test

--- src/service/Password.java
+++ src/service/Password.java
@@ -3,6 +3,10 @@ package service;
 public class Password {
 
     public void validatePassword(){
-        
+        // 2.Валидация password:
+        //   String password = "MySecurePassword123!";
+        boolean isValidPassword = password.matches("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&*!])[A-Za-z\\d@#$%^&*!]{8,}$");
+        //Это регулярное выражение проверяет, что пароль содержит
+        // как минимум одну букву, одну цифру и один специальный символ, и имеет длину не менее 8 символов
     }
 }
